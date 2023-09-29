00a298ff0426331d911b713dc2acfe0f24fd3456
Otávio Borges <otavio.demb@hotmail.com>
2023-09-28 22:32:06 -0300

    Aula 03: getServerSideProps para pegar id da URL.

--- desenvolvimento-frontend/meu-projeto-next/components/Post/index.tsx
+++ desenvolvimento-frontend/meu-projeto-next/components/Post/index.tsx
@@ -1,5 +1,4 @@
 import { IPost } from "@/types";
-import Image from "next/image";
 
 interface Props {
     post: IPost
@@ -8,7 +7,7 @@ interface Props {
 export default function Post({ post }:Props){
     return(
         <li>
-            <Image src={post.cover} alt={post.title} width={800} height={200}/>
+            <img src={post.cover} width={400} alt={post.title} />
             <h2>{ post.title }</h2>
             <p>{ post.body }</p>
         </li>
--- desenvolvimento-frontend/meu-projeto-next/pages/conteudo/[id].tsx
+++ desenvolvimento-frontend/meu-projeto-next/pages/conteudo/[id].tsx
@@ -1,5 +1,16 @@
-export default function Conteudo(){
+import { GetStaticProps } from "next"
+
+export async function getServerSideProps( context: { params: {id:number} } ) {
+    const { params } = context
+
+    return { props: {id:params.id} }
+}
+
+export default function Conteudo({ id }: {id:number}){
     return(
-        <div>Conteudo com id</div>
+        <>
+            <div>Conteudo com id</div>
+            <div>Id da página: {id}</div>
+        </>
     )
 }

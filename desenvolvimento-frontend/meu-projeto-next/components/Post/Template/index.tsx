0ff1d3c065426d0144267dfc9e3190744136ecc0
Otávio Borges <otavio.demb@hotmail.com>
2023-09-28 23:09:33 -0300

    Aula 03: global scss e Navbar;

--- desenvolvimento-frontend/meu-projeto-next/components/Post/Template/index.tsx
+++ desenvolvimento-frontend/meu-projeto-next/components/Post/Template/index.tsx
@@ -1,9 +1,11 @@
+import Navbar from '@/components/Navbar'
 import styles from './Template.module.scss'
 import React from 'react'
 
 export default function Template({ children }: { children: React.ReactElement }) {
     return(
         <div className={styles.main}>
+            <Navbar />
             {children}
         </div>
     )

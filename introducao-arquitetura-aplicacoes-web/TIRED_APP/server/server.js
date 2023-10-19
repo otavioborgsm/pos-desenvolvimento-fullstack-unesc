5ecd323830e6f24c1b36ad0e2068d27625871342
Otávio Borges <otavio.demb@hotmail.com>
2023-10-18 23:51:25 -0300

    Aula 02: Servidor Express Criado

--- introducao-arquitetura-aplicacoes-web/TIRED_APP/server/server.js
+++ introducao-arquitetura-aplicacoes-web/TIRED_APP/server/server.js
@@ -1,6 +1,11 @@
-const http = require('http');
-const server = http.createServer((_, res) => {
-  res.end('Hello World');
-});
- 
-server.listen(3000, '127.0.0.1', () => {});
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

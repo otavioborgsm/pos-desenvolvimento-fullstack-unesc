91feae7e6ac18581dd1df8ac29a264375a2b6049
Otávio Borges <otavio.demb@hotmail.com>
2023-10-18 23:39:44 -0300

    Aula 02: Inicio servidor Node.

---
+++ introducao-arquitetura-aplicacoes-web/TIRED_APP/server/server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const server = http.createServer((_, res) => {
+  res.end('Hello World');
+});
+ 
+server.listen(3000, '127.0.0.1', () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});

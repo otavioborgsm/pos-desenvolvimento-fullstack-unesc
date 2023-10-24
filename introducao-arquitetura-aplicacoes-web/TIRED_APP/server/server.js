2f25fe43a624b0b249bbf78166f0b025abf42580
Otávio Borges <otavio.demb@hotmail.com>
2023-10-23 23:05:32 -0300

    Aula 03: Aplica controle de CORS no servidor.

--- introducao-arquitetura-aplicacoes-web/TIRED_APP/server/server.js
+++ introducao-arquitetura-aplicacoes-web/TIRED_APP/server/server.js
@@ -1,11 +1,14 @@
 const express = require('express')
-const app = express()
+const cors = require('cors')
+const server = express()
 const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+sever.use(cors())
+
+server.get('/', (req, res) => {
+  res.json('Hello World!')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })

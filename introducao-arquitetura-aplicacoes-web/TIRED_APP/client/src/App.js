6f3b96e5729ec82c98555562bbda6514a5107651
Otávio Borges <otavio.demb@hotmail.com>
2023-10-24 22:11:18 -0300

    Aula 03: Faz conexão do cliente ao servidor.

--- introducao-arquitetura-aplicacoes-web/TIRED_APP/client/src/App.js
+++ introducao-arquitetura-aplicacoes-web/TIRED_APP/client/src/App.js
@@ -1,22 +1,26 @@
+import React, { useEffect, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [greeting, setGreeting] = useState("")
+
+
+  async function fetchGreeting() {
+    const response = await fetch('http://localhost:3001/')
+    setGreeting(await response.json())
+  }
+  
+  useEffect(() => {
+    fetchGreeting();
+  }, [greeting]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{ greeting } + React </p>
       </header>
     </div>
   );

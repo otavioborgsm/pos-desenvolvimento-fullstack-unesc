b7f6bf5dc265b918a2bdbac0c8951e533771f1a0
Otávio Borges <otavio.demb@hotmail.com>
2023-12-05 22:54:33 -0300

    Aula 03: Adiciona consulta de CEP.

--- fundamentos-desenvolvimento-frontend/bootstrap-project/app.js
+++ fundamentos-desenvolvimento-frontend/bootstrap-project/app.js
@@ -1,14 +1,35 @@
 (function(){
     window.addEventListener('load', () => {
-        let formCadastroUsuario = document.getElementById("formCadastroUsuario")
-        formCadastroUsuario.addEventListener('submit', event => {
-            if (!formCadastroUsuario.checkValidity()) {
-              event.preventDefault();
-              event.stopPropagation();
-              // submit
+        let formConsultaCep = document.getElementById("formConsultaCep")
+        formConsultaCep.addEventListener('submit', async (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            formConsultaCep.classList.add("was-validated");
+
+            if (formConsultaCep.checkValidity()) {
+                let cepInput = document.querySelector('input[name=cep]')
+
+                let url = `https://viacep.com.br/ws/${cepInput.value}/json/`
+                await fetch(url)
+                    .then(response => {
+                        return response.json()
+                    })
+                    .then((data) => {
+                        let logradouroInput = document.querySelector('input[name=logradouro]')
+                        let bairroInput = document.querySelector('input[name=bairro]')
+                        let localidadeInput = document.querySelector('input[name=localidade]')
+                        let ufInput = document.querySelector('input[name=uf]')
+
+                        logradouroInput.value = data.logradouro
+                        bairroInput.value = data.bairro
+                        localidadeInput.value = data.localidade
+                        ufInput.value = data.uf
+                    })
+                    .catch(error => {
+                        console.error(error)
+                    })
             }
 
-            formCadastroUsuario.classList.add("was-validated");
         }, false)
     })
 })();

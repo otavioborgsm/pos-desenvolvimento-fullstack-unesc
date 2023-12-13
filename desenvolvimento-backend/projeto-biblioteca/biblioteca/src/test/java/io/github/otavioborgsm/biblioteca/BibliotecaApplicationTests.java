e39308ebf210d38eda0e61c8d30a487115d5de6e
Otávio Borges <otavio.demb@hotmail.com>
2023-12-12 21:00:00 -0300

    Aula 01: Cria aplicação Spring Boot.

---
+++ desenvolvimento-backend/projeto-biblioteca/biblioteca/src/test/java/io/github/otavioborgsm/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.otavioborgsm.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d843f8d673afff5324c60f629a14382b21a4461e
Otávio Borges <otavio.demb@hotmail.com>
2023-12-18 23:45:50 -0300

    Aula 03: Adiciona Modelo de Alunos

--- desenvolvimento-backend/biblioteca-django/biblioteca/alunos/models.py
+++ desenvolvimento-backend/biblioteca-django/biblioteca/alunos/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Aluno(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    matricula = models.CharField(max_length=20)

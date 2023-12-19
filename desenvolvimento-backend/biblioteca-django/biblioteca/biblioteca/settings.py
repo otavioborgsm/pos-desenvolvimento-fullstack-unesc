2b81ce046f46ad6e824b1e449d85bc7b25d2c8ee
Otávio Borges <otavio.demb@hotmail.com>
2023-12-18 23:38:58 -0300

    Aula 03: Configura conexão com base PostgreSQL

--- desenvolvimento-backend/biblioteca-django/biblioteca/biblioteca/settings.py
+++ desenvolvimento-backend/biblioteca-django/biblioteca/biblioteca/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'biblioteca.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'biblioteca-django',
+        'USER': 'postgres',
+        'PASSWORD': 'postgresmanager',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 

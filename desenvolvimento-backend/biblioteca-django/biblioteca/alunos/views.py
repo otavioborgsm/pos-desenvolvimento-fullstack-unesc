c6440dd58bb15dc1bafff78b4426f3317b563c5d
Otávio Borges <otavio.demb@hotmail.com>
2023-12-19 10:56:27 -0300

    Aula 03: Adiciona autenticação  na API de Alunos

--- desenvolvimento-backend/biblioteca-django/biblioteca/alunos/views.py
+++ desenvolvimento-backend/biblioteca-django/biblioteca/alunos/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .serializers import AlunoSerializer
 from .models import Aluno
 
 class AlunoViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+
     serializer_class = AlunoSerializer
     queryset = Aluno.objects.all()
 

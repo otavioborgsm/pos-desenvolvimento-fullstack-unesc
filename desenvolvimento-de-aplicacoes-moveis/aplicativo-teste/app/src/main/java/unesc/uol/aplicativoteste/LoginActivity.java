0f501b2202f5c4f7ed1c4ec0909b474523986fd2
otavioborgsm <otavio.demb@rede.ulbra.br>
2024-01-19 20:35:30 -0300

    Aula 01: Adiciona validações nos campos de login.

--- desenvolvimento-de-aplicacoes-moveis/aplicativo-teste/app/src/main/java/unesc/uol/aplicativoteste/LoginActivity.java
+++ desenvolvimento-de-aplicacoes-moveis/aplicativo-teste/app/src/main/java/unesc/uol/aplicativoteste/LoginActivity.java
@@ -1,15 +1,46 @@
 package unesc.uol.aplicativoteste;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText editEmail;
+    private EditText editSenha;
+    private Button btnEntrar;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editEmail = findViewById(R.id.editEmail);
+        editSenha = findViewById(R.id.editSenha);
+
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String conteudoEmail = editEmail.getText().toString();
+                String conteudoSenha = editSenha.getText().toString();
+
+                if (conteudoEmail.isEmpty()){
+                    editEmail.setError("Campo e-mail é obrigatório");
+                } else if (conteudoSenha.isEmpty()) {
+                    editSenha.setError("Campo senha é obrigatório");
+                } else {
+                    Toast.makeText(LoginActivity.this, "CAMPOS PREENCHIDOS", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
     }
 }

58e67d35492b2febabbcc5c4c47c005f63152163
otavioborgsm <otavio.demb@rede.ulbra.br>
2024-01-19 23:09:38 -0300

    Aula 01: Adiciona função de login na tela nova.

--- desenvolvimento-de-aplicacoes-moveis/aplicativo-teste/app/src/main/java/unesc/uol/aplicativoteste/LoginAlternativoActivity.java
+++ desenvolvimento-de-aplicacoes-moveis/aplicativo-teste/app/src/main/java/unesc/uol/aplicativoteste/LoginAlternativoActivity.java
@@ -1,12 +1,17 @@
 package unesc.uol.aplicativoteste;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputLayout;
+
+import unesc.uol.aplicativoteste.util.KeyUtil;
 
 public class LoginAlternativoActivity extends AppCompatActivity {
 
@@ -24,5 +29,33 @@ public class LoginAlternativoActivity extends AppCompatActivity {
         inputSenha = findViewById(R.id.inputSenha);
         btnEntrar = findViewById(R.id.btnEntrar);
 
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String conteudoEmail = inputEmail.getEditText().getText().toString();
+                String conteudoSenha = inputSenha.getEditText().getText().toString();
+
+                inputEmail.setErrorEnabled(false);
+                inputSenha.setErrorEnabled(false);
+
+                if (conteudoEmail.isEmpty()){
+                    inputEmail.setError("Campo e-mail é obrigatório");
+                    inputEmail.setErrorEnabled(true);
+                } else if (conteudoSenha.isEmpty()) {
+                    inputSenha.setError("Campo senha é obrigatório");
+                    inputSenha.setErrorEnabled(true);
+                } else {
+                    Toast.makeText(LoginAlternativoActivity.this, "CAMPOS PREENCHIDOS", Toast.LENGTH_LONG).show();
+
+                    Intent it = new Intent(LoginAlternativoActivity.this, PrincipalActivity.class);
+                    it.putExtra(KeyUtil.KEY_EMAIL, conteudoEmail);
+
+                    startActivity(it);
+                }
+
+            }
+        });
     }
 }

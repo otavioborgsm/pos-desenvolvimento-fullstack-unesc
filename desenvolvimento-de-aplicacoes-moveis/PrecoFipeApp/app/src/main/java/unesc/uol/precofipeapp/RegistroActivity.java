7bba2422978cfedc4e3d7a96539f999734870e23
otavioborgsm <otavio.demb@rede.ulbra.br>
2024-01-21 23:56:13 -0300

    Aula 02: Cria campos de texto na tela de registro.

--- desenvolvimento-de-aplicacoes-moveis/PrecoFipeApp/app/src/main/java/unesc/uol/precofipeapp/RegistroActivity.java
+++ desenvolvimento-de-aplicacoes-moveis/PrecoFipeApp/app/src/main/java/unesc/uol/precofipeapp/RegistroActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +13,8 @@ public class RegistroActivity extends AppCompatActivity {
 
     private Button btnSalvar;
     private Button btnCancelar;
+    private EditText editNomeUsuario;
+    private EditText editEmailUsuario;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,6 +39,9 @@ public class RegistroActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        editNomeUsuario = findViewById(R.id.editNomeUsuario);
+        editEmailUsuario = findViewById(R.id.editEmailUsuario);
 
     }
 }

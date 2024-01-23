b7241f315e3f160269423a34bbf8d992965ae874
otavioborgsm <otavio.demb@rede.ulbra.br>
2024-01-22 22:20:35 -0300

    Aula 02: Adiciona Texto de bem-vindo.

--- desenvolvimento-de-aplicacoes-moveis/PrecoFipeApp/app/src/main/java/unesc/uol/precofipeapp/activities/SplashActivity.java
+++ desenvolvimento-de-aplicacoes-moveis/PrecoFipeApp/app/src/main/java/unesc/uol/precofipeapp/activities/SplashActivity.java
@@ -2,10 +2,16 @@ package unesc.uol.precofipeapp.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +21,7 @@ public class SplashActivity extends AppCompatActivity {
 
     private ImageView imageAutomovel;
     private Animation animation;
+    private TextView textBemVindo;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,6 +29,7 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         imageAutomovel = findViewById(R.id.imageAutomovel);
+        textBemVindo = findViewById(R.id.textBemVindo);
 
         animation = AnimationUtils.loadAnimation(SplashActivity.this, R.anim.splash);
         animation.setAnimationListener(new Animation.AnimationListener() {
@@ -32,7 +40,15 @@ public class SplashActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                textBemVindo.setVisibility(View.VISIBLE);
+
+                final Handler handler = new Handler(Looper.getMainLooper());
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                    }
+                }, 5000);
             }
 
             @Override

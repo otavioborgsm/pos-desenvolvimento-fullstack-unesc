ebc6cc0cf3ca00e03b5853535e9d562212c31cf9
otavioborgsm <otavio.demb@rede.ulbra.br>
2024-01-23 21:53:40 -0300

    Aula 02: Cria Splash Screen animado.

---
+++ desenvolvimento-de-aplicacoes-moveis/PrecoFipeApp/app/src/main/java/unesc/uol/precofipeapp/activities/SplashAlternativaActivity.java
@@ -0,0 +1,39 @@
+package unesc.uol.precofipeapp.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+import unesc.uol.precofipeapp.R;
+
+public class SplashAlternativaActivity extends AppCompatActivity {
+
+    private LottieAnimationView animacaoVeiculo;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_alternativo);
+
+        animacaoVeiculo = findViewById(R.id.animacaoVeiculo);
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                animacaoVeiculo.setVisibility(View.GONE);
+
+                Intent it = new Intent(SplashAlternativaActivity.this, LoginActivity.class);
+                it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(it);
+            }
+        }, 9000);
+    }
+}

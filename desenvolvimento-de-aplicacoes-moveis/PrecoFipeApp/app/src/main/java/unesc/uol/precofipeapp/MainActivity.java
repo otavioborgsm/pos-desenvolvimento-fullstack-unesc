f7789dfa54fa44647533a40ad7126a289986f907
otavioborgsm <otavio.demb@rede.ulbra.br>
2024-01-23 22:41:42 -0300

    Aula 02: Cria lista simples de carros.

--- desenvolvimento-de-aplicacoes-moveis/PrecoFipeApp/app/src/main/java/unesc/uol/precofipeapp/MainActivity.java
+++ desenvolvimento-de-aplicacoes-moveis/PrecoFipeApp/app/src/main/java/unesc/uol/precofipeapp/MainActivity.java
@@ -3,12 +3,50 @@ package unesc.uol.precofipeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listaVeiculos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] dados = new String[]{
+                "Corsa",
+                "Focus",
+                "Tracker",
+                "Onix",
+                "Etios",
+                "Gol",
+                "Belina",
+                "Corsa",
+                "Focus",
+                "Tracker",
+                "Onix",
+                "Etios",
+                "Gol",
+                "Belina",
+                "Corsa",
+                "Focus",
+                "Tracker",
+                "Onix",
+                "Etios",
+                "Gol",
+                "Belina"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                dados
+            );
+
+        listaVeiculos = findViewById(R.id.listaVeiculos);
+        listaVeiculos.setAdapter(adapter);
+
     }
 }
